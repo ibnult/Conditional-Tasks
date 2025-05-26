d2bb11b40a983803cf902ddf989611ee27844f6f
ibnult <ibnultahsinrihan@gmail.com>
2025-05-27 00:43:48 +0600

    Task 5  complete

--- ternary-number.js
+++ ternary-number.js
@@ -1,5 +1,19 @@
+//if-else
 let num1 = 20;
 let num2 = 10;
+let result;
 
-let result = (num1 > num2) ? (num1 * 2) : (num1 + num2);
+if (num1 > num2) {
+  result = num1 * 2;
+} else {
+  result = num1 + num2;
+}
+
+console.log("Result:", result);
+
+//ternary
+let num3 = 20;
+let num4 = 10;
+
+let result2 = num3 > num4 ? num3 * 2 : num3 + num4;
 console.log("Result using ternary:", result);
